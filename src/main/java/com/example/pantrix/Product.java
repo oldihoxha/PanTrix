3ebb36984838dffa4d7514b006b3a0b47a1a2059
Nikolaos Pazartziklis <Nikolaos.Pazartziklis@student.htw-berlin.dewegit config listgit config --listgit config --global --listClearcleardeletecleargit config --global user.email Nikolaos.Pazartziklis@student.htw-berlin.degit config --global user.email Nikolaos.Pazartziklis@student.htw-berlin.degit config --global --list>
2025-10-15 16:34:05 +0200

    add Product.java :)

---
+++ src/main/java/com/example/pantrix/Product.java
@@ -0,0 +1,39 @@
+package com.example.pantrix;
+
+import java.time.LocalDate;
+
+
+public class Product {
+
+        private String name;
+        private LocalDate expiryDate;
+
+
+        public Product(){
+
+        }
+
+        public Product(String name, LocalDate expiryDate){
+            this.name = name;
+            this.expiryDate = expiryDate;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public LocalDate getExpiryDate(){
+            return expiryDate;
+        }
+
+        public void setExpiryDate(LocalDate expiryDate){
+            this.expiryDate = expiryDate;
+        }
+
+}
+
+
