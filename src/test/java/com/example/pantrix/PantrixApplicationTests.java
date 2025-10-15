fdb789fedd67928dc3041837f95b7130d3d22e32
oldihoxha <oldi.hoxha@student.htw-berlin.de>
2025-10-15 16:17:07 +0200

    added Spring initialized

---
+++ src/test/java/com/example/pantrix/PantrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pantrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
