e639ed1c506dcf29c30fbf654a1ed22c602d4107
oldihoxha <oldi.hoxha@student.htw-berlin.de>
2025-11-26 01:24:50 +0100

    added dependencies

--- src/main/java/com/example/pantrix/Product.java
+++ src/main/java/com/example/pantrix/Product.java
@@ -1,17 +1,24 @@
 package com.example.pantrix;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.Id;
+
 import java.time.LocalDate;
 
+@Entity
 
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
         private String name;
         private LocalDate expiryDate;
 
 
-        public Product(){
-
-        }
+        public Product(){}
 
         public Product(String name, LocalDate expiryDate){
             this.name = name;
